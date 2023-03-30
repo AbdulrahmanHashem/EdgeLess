fd6376887b2960f61c56c408e5b011a072a4d172
Abdulrahman <abdalrhmannts@gmail.com>
2023-03-30 15:05:18 +0200

    - fixed connection and reconnection loop.

--- EdgeLess.py
+++ EdgeLess.py
@@ -9,7 +9,7 @@ from Application.UserInterface.MainUI import EdgeLess
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    app.setWindowIcon(QIcon("Application/.Resources/EdgeLess_Logo.ico"))
+    app.setWindowIcon(QIcon("Resources/EdgeLess_Logo.ico"))
 
     myappid = 'NTS.EdgeLess.EdgeLess.1.0'
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
